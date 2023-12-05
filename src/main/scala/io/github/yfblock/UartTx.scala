144c95e6c5be3c1e43bd1d3f436a07812bd51c54
yufeng <321353225@qq.com>
2023-12-05 13:09:24 +0800

    uart tx first version

--- src/main/scala/io/github/yfblock/UartTx.scala
+++ src/main/scala/io/github/yfblock/UartTx.scala
@@ -2,20 +2,38 @@ package io.github.yfblock
 
 import spinal.core._
 import scala.language.postfixOps
+import spinal.lib.fsm.StateMachine
+import spinal.lib.fsm.EntryPoint
+import spinal.lib.fsm.State
+import spinal.lib.fsm.StateDelay
+import spinal.lib.Counter
 
 /// url: https://github.com/xiaotianbc/spinalHDL-uart-demo.git
 object UartTx {
     def apply(txp: Bool): UartTx = {
         val uartTx = new UartTx()
+        val datas = Vec("Hello\r\n".map(c => B(c.toInt, 8 bits)))
+        val index = Reg(UInt(log2Up(datas.length) bits)).init(0)
+        uartTx.io.tx_en := index < datas.length
+        when(~uartTx.io.is_busy && uartTx.io.tx_en)(index := index + 1)
+        uartTx.io.tx_data := datas(index)
         uartTx.io.txp <> txp
-        uartTx.io.tx_en := True
-        uartTx.io.tx_data := '3'
+        // uartTx.io.tx_en := True
+        // uartTx.io.tx_data := '3'
+        val cnt = UInt(32 bits).setAsReg().init(0)
+        cnt := cnt + 1
+        when(cnt === 27000000) {
+          when(index === datas.length) {
+            index := 0
+          }
+          cnt := 0
+        }
+        Counter
         uartTx
     }
 }
 
 class UartTx(baudrate: Int = 115200) extends Component {
-//   val BAUD_CNT_MAX = (clk_freq / baudrate) - 1
   val BAUD_CNT_MAX = (this.clockDomain.frequency.getValue.toInt / baudrate) - 1
   val baud_cnt = Reg(UInt(log2Up(BAUD_CNT_MAX) bits)) init 0
 
@@ -31,35 +49,39 @@ class UartTx(baudrate: Int = 115200) extends Component {
   val tx_data_r = Reg(Bits(10 bits)) init 0
   val tx_bits_counter = Reg(UInt(log2Up(10) bits)) init 0 //定义一个宽度为log2Up(8)的无符号多位寄存器
 
-  object State extends SpinalEnum {
-    val idle, working = newElement()
-  }
+  io.is_busy := True
 
-  val state = RegInit(State.idle)
-
-  //这里如果tx_en被置位则也让busy置位
-  io.is_busy := (!(state === State.idle)) || io.tx_en
-
-  switch(state) {
-    is(State.idle) {
-      io.txp := True
-      when(io.tx_en) {
-        //使用 ## 对Bits类型进行拼接，前面的是MSB，后面的是LSB。因为最先发送起始位，所以最低位为0
-        tx_data_r := B"1" ## io.tx_data ## B"0"
-        baud_cnt := 0
-        tx_bits_counter := 0
-        state := State.working
+  val fsm = new StateMachine {
+    val IDLE: State = new State with EntryPoint {
+      whenIsActive {
+        io.is_busy := False
+        io.txp := True
+        when(io.tx_en) {
+          tx_data_r := B"1" ## io.tx_data ## B"0"
+          baud_cnt := 0
+          tx_bits_counter := 0
+          goto(WORKING)
+        }
       }
     }
-    is(State.working) {
-      io.txp := tx_data_r(tx_bits_counter)
-      baud_cnt := baud_cnt + 1
-      when(baud_cnt === BAUD_CNT_MAX) {
-        baud_cnt := 0
-        tx_bits_counter := tx_bits_counter + 1
-        when(tx_bits_counter === 9) {
-          state := State.idle
+
+    val WORKING = new State {
+      whenIsActive {
+        io.txp := tx_data_r(tx_bits_counter)
+        baud_cnt := baud_cnt + 1
+        when(baud_cnt === BAUD_CNT_MAX) {
+          baud_cnt := 0
+          tx_bits_counter := tx_bits_counter + 1
+          when(tx_bits_counter === 9) {
+            goto(END)
+          }
         }
+      }
+    }
+
+    val END = new StateDelay(5) {
+      whenCompleted {
+        goto(IDLE)
       }
     }
   }
